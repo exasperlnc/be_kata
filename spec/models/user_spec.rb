c3754855c6a0b29268eb41e9d1b69bbb06c2599a
Logan Cole <emtlogancole@gmail.com>
2023-10-12 12:55:53 -0500

    test user attributes and existence

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 describe User do
-  
+  let(:users) { create_list(:user, 3) }
+  let(:user1) { users.first }
+  it 'exists' do
+    expect(user1).to be_a(User)
+  end
+
+  it 'has attributes' do
+    users.each do |user|
+      expect(user.f_name).to_not eq(nil)
+      expect(user.l_name).to_not eq(nil)
+      expect(user.email).to_not eq(nil)
+      expect(user.password_digest).to_not eq(nil)
+    end
+  end
 end
