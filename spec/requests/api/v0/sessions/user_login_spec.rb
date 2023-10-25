f07ede93eed5c2c1e9bf4983818ce505a3f7b111
Logan Cole <emtlogancole@gmail.com>
2023-10-25 13:03:38 -0500

    add sad path testing to user login

--- spec/requests/api/v0/sessions/user_login_spec.rb
+++ spec/requests/api/v0/sessions/user_login_spec.rb
@@ -7,6 +7,9 @@ RSpec.describe 'login request' do
     post api_v0_login_path, params: { session: { email: user1.email, password: user1.password }} 
     data = JSON.parse(response.body, symbolize_names: true)
     
+    expect(session).to have_key(:user_id)
+    expect(session[:user_id]).to eq(user1.id)
+
     expect(data).to have_key(:f_name) 
     expect(data[:f_name]).to be_a(String) 
 
@@ -20,6 +23,12 @@ RSpec.describe 'login request' do
   end
 
   it 'sad path' do
+    post api_v0_login_path, params: { session: { email: "incorrect@email.com", password: "not a password" }} 
+    data = JSON.parse(response.body, symbolize_names: true)
     
+    expect(data).to have_key(:error)
+    expect(data[:error]).to eq("Invalid Credentials")
+
+    expect(session[:user_id]).to eq(nil)
   end
 end
