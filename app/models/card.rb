e6759db72e89418762791f6e372dd89551741921
Logan Cole <emtlogancole@gmail.com>
2023-10-25 14:06:53 -0500

    fix card/deck/user relationship

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,4 @@
 class Card < ApplicationRecord
   belongs_to :deck
+  has_one :user, through: :deck
 end
--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe Card do
   describe 'relationships' do
     it { should belong_to(:deck) }
-    it { should belong_to(:user).through(:deck) }
+    it { should have_one(:user).through(:deck) }
   end
 end
